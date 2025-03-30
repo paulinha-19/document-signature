d31e0536c7f14ed64be2f64390b6db1d3e05d7dc
paulinha-19 <paulafabianasoares@gmail.com>
2025-03-30 17:41:24 -0300

    [add] registerFormSchema - zod

--- app/schemas/auth.ts
+++ app/schemas/auth.ts
@@ -5,4 +5,15 @@ export const loginFormSchema = z.object({
   password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
 })
 
+export const registerFormSchema = z.object({
+  name: z.string().min(1, 'Insira seu nome'),
+  email: z.string().email('Insira seu email'),
+  password: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
+  confirmPassword: z.string().min(6, 'A senha deve ter pelo menos 6 caracteres'),
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Senhas diferentes",
+  path: ['confirmPassword'],
+})
+
 export type LoginFormData = z.infer<typeof loginFormSchema>
+export type RegisterFormData = z.infer<typeof registerFormSchema> 
