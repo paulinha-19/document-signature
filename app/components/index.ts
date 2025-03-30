6aaff93a78db34938a7368adcb377e30a5a58e1e
paulinha-19 <paulafabianasoares@gmail.com>
2025-03-30 17:30:35 -0300

    [add] export components - index.ts

---
+++ app/components/index.ts
@@ -0,0 +1,9 @@
+import { Button } from "./Button";
+import { ButtonOAuth } from "./ButtonOAuth";
+import { Divider } from "./Divider";
+import { FooterForm } from "./FooterForm";
+import { FormInput } from "./FormInput";
+import { Input } from "./Input";
+import { Logo } from "./Logo";
+
+export { Button, ButtonOAuth, Divider, FooterForm, FormInput, Input, Logo }
