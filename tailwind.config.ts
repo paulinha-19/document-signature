14cda14fa94c5ea9434d478c6c9b90c8a6e19cef
paulinha-19 <paulafabianasoares@gmail.com>
2025-03-30 17:29:15 -0300

    [update] default colors - tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,30 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: '#E8F7EC',
+          100: '#D1F0D9',
+          200: '#A3E1B3',
+          300: '#75D28D',
+          400: '#47C367',
+          500: '#30A949', // base color
+          600: '#268739',
+          700: '#1C652A',
+          800: '#13431C',
+          900: '#09220E',
+        },
+        secondary: {
+          50: '#F2E8F7',
+          100: '#E5D1F0',
+          200: '#CBA3E1',
+          300: '#B175D2',
+          400: '#9747C3',
+          500: '#592084', // base color
+          600: '#471A6A',
+          700: '#35134F',
+          800: '#240D35',
+          900: '#12061A',
+        },
       },
     },
   },
